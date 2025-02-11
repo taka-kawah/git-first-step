bc4ada2d63c0603919112238bf2d590765268ad3
taka-kawah <kawaharatakashi0724@gmail.com>
2025-02-11 11:28:44 +0900

    練習1

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-    for i := 1; i <= 100; i++ {
-        if i%3 == 0 && i%5 == 0 {
-            fmt.Println("FizzBuzz")
-        } else if i%3 == 0 {
-            fmt.Println("Fizz")
-        } else if i%5 == 0 {
-            fmt.Println("Buzz")
-        } else {
-            fmt.Println(i)
-        }
-    }
+	p1()
+}
+
+func p1() {
+	f := 1.11
+	numf := int(f)
+	fmt.Println(numf)
+	fmt.Println("numfの型は...", reflect.TypeOf(numf))
+
+	s := []int{1, 2, 5, 6, 2, 3, 1}
+	fmt.Println(s[2:4])
+
+	m := map[string]int{
+		"Mike":  20,
+		"Nancy": 24,
+		"Messi": 30,
+	}
+	fmt.Printf("%T %v", m, m)
 }
